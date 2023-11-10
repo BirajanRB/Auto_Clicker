a6891da70de9dadf9f458662e13b1478139d61b1
Birajan <BirajanRB@gmail.com>
2023-11-10 17:34:55 +0545

    Added new toggle keys

--- Mouse Auto Clicker.py
+++ Mouse Auto Clicker.py
@@ -2,7 +2,19 @@ import mouse
 import keyboard
 import time
 
+toggle = False
+
 while True:
     if keyboard.is_pressed('p'):
         mouse.click(button='left')
-    time.sleep(0.005)
+    
+    if keyboard.is_pressed('i'):
+        toggle = not(toggle)
+    
+    if keyboard.is_pressed('o'):
+        toggle = False
+
+    if toggle:
+        mouse.click(button='left')
+        
+    time.sleep(0.003)
